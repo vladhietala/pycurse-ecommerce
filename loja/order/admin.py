1cf163cd9ed4218c792f3cc3c0014682ed2a6295
Vlad Hietala <vlad.hietala@hotmail.com>
2022-02-24 03:57:16 -0300

    execute pre-commit on all files

--- loja/order/admin.py
+++ loja/order/admin.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
--- loja/profile/models.py
+++ loja/profile/models.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Profile(models.Model):
+    class Meta:
+        verbose_name = _("Profile")
+        verbose_name_plural = _("Profiles")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Profile_detail", kwargs={"pk": self.pk})
